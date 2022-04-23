9c9c0fec949c18bf1fb7f0f56e9edcf8a7230206
rosenrose <crazytempler@gmail.com>
2022-04-23 17:36:05 +0900

    init

---
+++ build/index.js
@@ -0,0 +1,15 @@
+const hello = () => "hi";
+function hi() {
+    return "hello";
+}
+class Block {
+    constructor(data) {
+        this.data = data;
+    }
+    static hi() {
+        return "hi";
+    }
+}
+const a = [1, 2, 3];
+const b = [...a, Math.pow(4, 5)];
+const c = 1000000;
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const hello = () => "hi";
+function hi() {
+  return "hello";
+}
+
+class Block {
+  constructor(private data: string) {}
+  static hi() {
+    return "hi";
+  }
+}
+
+const a = [1, 2, 3];
+const b = [...a, 4 ** 5];
+const c = 1_000_000;
+
+import { init, exit } from "myPackage";
+init();
