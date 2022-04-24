7ea7c767afd388e5e4329fd1e77bfc0e53e20626
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-04-24 20:24:15 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,3 @@
-const hello = () => "hi";
-function hi() {
-  return "hello";
-}
-
-class Hello {
-  constructor(private data: string) {}
-  static hi() {
-    return "hi";
-  }
-}
-
-const a = [1, 2, 3];
-const b = [...a, 4 ** 5];
-const c = 1_000_000;
-
-// import { init, exit } from "myPackage"; // from package
-// init({ url: "" });
-// exit(1);
-
-// import { init2, exit2 } from "./myPackage2"; // from file
-// init2({ url: "" });
-// exit2(1);
-
 import crypto from "crypto";
 
 interface BlockShape {
